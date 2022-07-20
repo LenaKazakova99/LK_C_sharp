9fc0c9d64fbfb953f5184821d6fd1d30daa7a484
elena <lena.kazakova99@gmail.com>
2022-07-20 21:55:17 +0400

    Добавили в 4 виде цикл for

--- Examples_02.07.22/example1/Program.cs
+++ Examples_02.07.22/example1/Program.cs
@@ -38,18 +38,39 @@ int Year = Method3();
 
 
 // Вид 4
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
+// string Method4(int count, string text)
+// {
+//    int i = 0;
+//    string result = String.Empty;
+//
+//    while (i < count)
+//    {
+//        result = result + text;
+//        i++;
+//    }
+//    return result;
+//}
+//
 
-    while (i < count)
+string Method4(int count, string text)
+ {
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 
 string res = Method4(10, "z");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
